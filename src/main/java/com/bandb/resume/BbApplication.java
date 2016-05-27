2ac86a69ad044d23fbd0246f9136d1771b26685d
Bhanu Shankar Pottipareddy <bhanushankar422@gmail.com>
2016-05-26 23:24:13 -0500

    Initial commit

---
+++ src/main/java/com/bandb/resume/BbApplication.java
@@ -0,0 +1,12 @@
+package com.bandb.resume;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BbApplication.class, args);
+	}
+}
